5fc9551fdecbfd307a7ce401b30aa51217eed56c
Harrison Lavin <Harrison.lavin@gmail.com>
2016-01-02 01:49:45 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
 
 def assign_variable(value)
-  
+  desired_variable= value
 end
 
-def argue
-  
+def argue(arg1)
+  arg1
 end
 
-def greeting
-
+def greeting(name, language)
+  puts("Hi #{name}, I hear you're learning #{language}")
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  1+1
+  4/2
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+  topping
 end
